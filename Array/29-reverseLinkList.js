31b98d9d588fe993e6e75847b23205b829ad1d3c
sauravrawat99 <rawatsaurav145@gmail.com>
2026-05-18 08:08:25 +0530

    reverse linklist

---
+++ Array/29-reverseLinkList.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  print() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += current.data + " -> ";
+      current = current.next;
+    }
+    console.log(result + "null");
+  }
+
+  reverse() {
+    let prev = null;
+    let curr = this.head;
+
+    while (curr) {
+      [curr.next, prev, curr] = [prev, curr, curr.next];
+    }
+
+    this.head = prev;
+  }
+}
+
+const list = new LinkList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+list.print();
+list.reverse();
+list.print();
