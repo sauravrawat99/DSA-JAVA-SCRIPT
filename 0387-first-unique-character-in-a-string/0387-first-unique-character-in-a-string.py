240b89e32139465b5d782ce2e079ab3506d9456d
sauravrawat99 <rawatsaurav145@gmail.com>
2026-08-01 14:53:09 +0530

    Time: 38 ms (96.44%), Space: 19.9 MB (6.45%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -3,6 +3,6 @@ class Solution:
         s_count=Counter(s)
 
         for i in range(len(s)):
-            if s_count[s[i]]<=1:
-                return i
+           if s_count[s[i]] == 1:
+               return i
         return -1
