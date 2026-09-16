5bf17c4d2a0b418295486e1e6cc6767ed5a8db89
sauravrawat99 <rawatsaurav145@gmail.com>
2026-09-16 09:59:38 +0530

    Time: 40 ms (36.7%), Space: 42.4 MB (73.46%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,20 +5,50 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: ListNode | None) -> bool:
-        temp=head
-        arr=[]
-        while temp:
-            arr.append(temp.val)
-            temp=temp.next
+        # brute forece using extra space O(n)
+        # temp=head
+        # arr=[]
+        # while temp:
+        #     arr.append(temp.val)
+        #     temp=temp.next
 
-        st=0
-        end=len(arr)-1
+        # st=0
+        # end=len(arr)-1
 
-        while st<end:
-            if arr[st] != arr[end]:
-                return False
-            st +=1
-            end -=1
-        return True        
+        # while st<end:
+        #     if arr[st] != arr[end]:
+        #         return False
+        #     st +=1
+        #     end -=1
+        # return True      
+
+        # optimize with constant space O(1)
 
         
+        slow = fast = head
+
+        # find middle
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second half
+        prev = None
+        curr = slow
+
+        while curr:
+            new_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = new_node
+
+        # compare left and right half
+        left = head
+        right = prev
+
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
