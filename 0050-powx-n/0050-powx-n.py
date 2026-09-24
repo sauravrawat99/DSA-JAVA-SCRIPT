d9c78fb00a7fc074a790fa4b1ee8cea5166aadec
sauravrawat99 <rawatsaurav145@gmail.com>
2026-09-24 09:31:54 +0530

    Time: 0 ms (100%), Space: 19.7 MB (21.63%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,14 +1,31 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n<0:
-            x=1/x
-            n=-n
+        # optimize time o(logn)
+        # if n<0:
+        #     x=1/x
+        #     n=-n
 
-        result=1
-        while n>0:
-            if n%2==1:
-                result*=x
-            x=x*x
-            n=n//2    
+        # result=1
+        # while n>0:
+        #     if n%2==1:
+        #         result*=x
+        #     x=x*x
+        #     n=n//2    
         
-        return result
+        # return result
+        def helper(x,n):
+            if n<0:
+                 x=1/x
+                 n=-n
+
+            if n==0:
+                 return 1
+            
+            half=helper(x,n//2)
+
+            if n%2==0:
+                return half*half
+            else:
+                return half*half*x
+                
+        return helper(x,n)            
